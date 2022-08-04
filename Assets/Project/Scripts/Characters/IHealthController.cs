bf61153bb9cf942f1d2ef0f934c443def8c51f06
TheOctan <kanatush.tim@gmail.com>
2022-08-04 22:40:26 +0300

    Health controller

---
+++ Assets/Project/Scripts/Characters/IHealthController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface IHealthController
+{
+    event Action OnDie;
+    event Action<int> OnDamaged;
+    int MaxHealth { get; }
+    int CurrentHealth { get; }
+    void TakeDamage(int damage);
+}
---
+++ Assets/Project/Scripts/Enemy/HealthController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project.Scripts.Enemy
+{
+    public class HealthController : MonoBehaviour, IHealthController
+    {
+        public event Action OnDie;
+        public event Action<int> OnDamaged;
+
+        [SerializeField, Min(1)] private int _maxHealth;
+        [SerializeField, Min(0)] private int _startHealth;
+
+        public int MaxHealth => _maxHealth;
+        public int CurrentHealth { get; private set; }
+
+        private void OnValidate()
+        {
+            if (_startHealth > _maxHealth)
+            {
+                _startHealth = _maxHealth;
+            }
+        }
+
+        private void Awake()
+        {
+            CurrentHealth = _startHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            CurrentHealth -= damage;
+            OnDamaged?.Invoke(damage);
+
+            if (CurrentHealth <= 0)
+            {
+                CurrentHealth = 0;
+                OnDie?.Invoke();
+            }
+        }
+    }
+}
