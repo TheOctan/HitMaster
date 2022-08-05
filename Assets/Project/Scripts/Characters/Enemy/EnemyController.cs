e767b6f54f01b5913174e17536735ce6b2498ed5
TheOctan <kanatush.tim@gmail.com>
2022-08-05 13:15:20 +0300

    Platform group

---
+++ Assets/Project/Scripts/Characters/Enemy/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[SelectionBase]
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Transform _target;
+
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void SetFollowTarget(Transform target)
+    {
+        _target = target;
+    }
+
+    public void StartFollowing()
+    {
+        
+    }
+
+    public void StopFollowing()
+    {
+        
+    }
+}
--- Assets/Project/Scripts/Enemy/EnemyController.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Project.Scripts.Enemy
-{
-    public class EnemyController : MonoBehaviour
-    {
-        
-    }
-}
