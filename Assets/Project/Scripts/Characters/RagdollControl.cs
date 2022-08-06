7e7a6b9e1ab90ea24b2e2eb4071f868b41758ad9
TheOctan <kanatush.tim@gmail.com>
2022-08-06 18:04:03 +0300

    Enemy ragdoll effect

---
+++ Assets/Project/Scripts/Characters/RagdollControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RagdollControl : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody[] _rigidbodies;
+
+    private void Awake()
+    {
+        foreach (Rigidbody rigidbodyComponent in _rigidbodies)
+        {
+            rigidbodyComponent.isKinematic = true;
+        }
+    }
+
+    [ContextMenu("SwitchToPhysical")]
+    public void SwitchToPhysical()
+    {
+        _animator.enabled = false;
+        foreach (Rigidbody rigidbodyComponent in _rigidbodies)
+        {
+            rigidbodyComponent.isKinematic = false;
+        }
+    }
+}
