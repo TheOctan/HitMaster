061f794973957be9abf24dde3f081f5fec41fc8c
TheOctan <kanatush.tim@gmail.com>
2022-08-07 21:33:03 +0300

    Game over screen

--- Assets/Project/Scripts/LevelManager.cs
+++ Assets/Project/Scripts/LevelManager.cs
@@ -6,6 +6,7 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField] private PlatformGroupManager _platformGroupManager;
     [SerializeField] private ProgressBarView _progressBarView;
+    [SerializeField] private GameOverScreenView _gameOverScreenView;
 
     private Transform _target;
     private List<Transform> _enemies;
@@ -13,12 +14,22 @@ public class LevelManager : MonoBehaviour
 
     private void Start()
     {
-        _target = _platformGroupManager.Target;
-        if (_target.TryGetComponent(out IPlayer player))
-        {
-            player.OnDie += OnPlayerDieHandler;
-        }
+        InitPlayerTarget();
+        InitEnemies();
+        InitProgressBar();
 
+        _gameOverScreenView.OnRestartButtonClicked += OnRestartButtonClickedHandler;
+    }
+
+    private void InitProgressBar()
+    {
+        int enemiesCount = _enemies.Count;
+        _progressBarView.MaximumValue = enemiesCount;
+        _progressBarView.SetValue(0);
+    }
+
+    private void InitEnemies()
+    {
         _enemies = _platformGroupManager.GetEnemies().ToList();
         foreach (Transform enemyTransform in _enemies)
         {
@@ -27,20 +38,30 @@ public class LevelManager : MonoBehaviour
                 enemy.OnDie += OnEnemyDieHandler;
             }
         }
+    }
 
-        int enemiesCount = _enemies.Count;
-        _progressBarView.MaximumValue = enemiesCount;
-        _progressBarView.SetValue(0);
+    private void InitPlayerTarget()
+    {
+        _target = _platformGroupManager.Target;
+        if (_target.TryGetComponent(out IPlayer player))
+        {
+            player.OnDie += OnPlayerDieHandler;
+        }
     }
 
     private void OnPlayerDieHandler()
     {
-        
+        _gameOverScreenView.gameObject.SetActive(true);
     }
 
     private void OnEnemyDieHandler()
     {
         _killedEnemiesCount++;
         _progressBarView.SetValue(_killedEnemiesCount);
+    }
+
+    private void OnRestartButtonClickedHandler()
+    {
+        
     }
 }
